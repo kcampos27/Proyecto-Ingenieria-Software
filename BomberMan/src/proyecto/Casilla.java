2990b305185049e578c42d8fe46e363f9d5a53a3
AdrianV3 <avisan2000@gmail.com>
2025-02-26 17:18:19 +0100

    Update Casilla.java

--- BomberMan/src/proyecto/Casilla.java
+++ BomberMan/src/proyecto/Casilla.java
@@ -1,27 +1,38 @@
 package proyecto;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Casilla extends JPanel
-{
-	//ATRIBUTOS
-	private int x;
-	private int y;
+public class Casilla {
 	
-	//CONSTRUCTORA
-	public Casilla (int pX, int pY)
-	{
-		super();
-		x=pX;
-		y=pY;
-	}
-	
-	//METODOS
-	public int getX()
-	{
-		return x;
-	}
-	public int getY()
-	{
-		return y;
-	}
+   //ATRIBUTOS
+    private int x;
+    private int y;
+    private String contenido; 
+
+    // CONSTRUCTORA
+    public Casilla(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.contenido = "libre";  
+    }
+
+    // METODOS
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getEstado() {
+        return contenido;
+    }
+
+    public void setEstado(String estado) {
+        this.contenido = estado;
+    }
 }
+
