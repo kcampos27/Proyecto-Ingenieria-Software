19cd0cd22217018f61bb96b3773f8ca656e049f5
AdrianV3 <avisan2000@gmail.com>
2025-02-26 18:49:06 +0100

    Update compatibilidad BomberMan.java

--- BomberMan/src/proyecto/BomberMan.java
+++ BomberMan/src/proyecto/BomberMan.java
@@ -1,22 +1,33 @@
 package proyecto;
 
-public class BomberMan {
-    private int x, y; // Posición en la cuadrícula
-
-    public BomberMan(int startX, int startY) {
-        this.x = startX;
-        this.y = startY;
-    }
-
-    public int getX() { return x; }
+public class BomberMan extends Contenido{
+	
+	//ATRIBUTOS
+	private static BomberMan miBomberman;
+	private int x, y; // Posicion en la cuadricula
+	
+	//CONSTRUCTORA
+	private BomberMan(int startX, int startY)
+	{
+		super("bomberman");
+		x = startX;
+		y= startY;
+	}
+	
+	//METODOS
+	public int getX() { return x; }
     public int getY() { return y; }
-
-    public void mover(int dx, int dy, int maxCols, int maxRows) {
+	public static BomberMan getBomberman()
+	{
+		return miBomberman;
+	}
+	public void mover(int dx, int dy, int maxCols, int maxRows) {
         int nuevoX = x + dx;
         int nuevoY = y + dy;
 
-        // Evita que salga del tableru
+        // Evita que salga del tablero
         if (nuevoX >= 0 && nuevoX < maxCols) x = nuevoX;
         if (nuevoY >= 0 && nuevoY < maxRows) y = nuevoY;
     }
+
 }
