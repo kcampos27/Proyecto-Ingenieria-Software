00eb3cc3dcf5c5a0f9417ee85068fc0573afeb7d
AntiaAR <arean.antia@gmail.com>
2025-03-20 12:15:46 +0100

    Update BombaSuper.java

--- src/model/BombaSuper.java
+++ src/model/BombaSuper.java
@@ -5,5 +5,12 @@ public class BombaSuper extends Bombas {
 	public BombaSuper(int x, int y) { //son las coordenadas de la bomba y num de celda que destruye
 		super(x,y,1);
 	}
+	public Bombas crearBombas() {
+		BomberMan bomberman = BomberMan.getMiBomberMan();
+		int bombX=bomberman.getX();
+		int bombY=bomberman.getY();
+		BombaSuper bomba = new BombaSuper(bombX,bombY);
+		return bomba;
+	}
 }
 
