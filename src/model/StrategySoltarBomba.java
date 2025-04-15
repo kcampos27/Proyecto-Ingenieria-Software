cc26ae40802f9e3efdaee1591f6ed95c25b3c7e4
Keneth <kcampos003@ikasle.ehu.eus>
2025-04-15 18:17:48 +0200

    Strategys

---
+++ src/model/StrategySoltarBomba.java
@@ -0,0 +1,5 @@
+package model;
+
+public interface StrategySoltarBomba {
+    void soltarBomba(BomberMan pBomber);
+}
---
+++ src/model/StrategyUltra.java
@@ -0,0 +1,32 @@
+package model;
+
+public class StrategyUltra implements StrategySoltarBomba{
+    public void soltarBomba(BomberMan pBomber) {
+        if (pBomber.tipo.equals("blanco")){
+            if (pBomber.bombasActivas < 1 &&
+                    (Gestor.getInstance().getTablero().casillaIncluye(pBomber.x, pBomber.y, "") ||
+                            Gestor.getInstance().getTablero().casillaIncluye(pBomber.x, pBomber.y, pBomber.nombre))) {
+                Gestor.getInstance().getTablero().eliminarContent(pBomber.x, pBomber.y, pBomber.nombre);
+                pBomber.cambiarTipo("bomberBomba");
+                Gestor.getInstance().getTablero().aniadirContent(pBomber.x, pBomber.y, pBomber);
+                pBomber.cambiarTipo("bombermanW");
+                Gestor.getInstance().getTablero().aniadirContent(pBomber.x, pBomber.y, new BombaUltra(pBomber.x, pBomber.y));
+                pBomber.bombasActivas++;
+                System.out.println("BOMBA ULTRA colocada. Activas: " + pBomber.bombasActivas);
+            }
+        }
+        else if (pBomber.tipo.equals("negro")){
+            if (pBomber.bombasActivas < 1 &&
+                    (Gestor.getInstance().getTablero().casillaIncluye(pBomber.x, pBomber.y, "") ||
+                            Gestor.getInstance().getTablero().casillaIncluye(pBomber.x, pBomber.y, pBomber.nombre))) {
+                Gestor.getInstance().getTablero().eliminarContent(pBomber.x, pBomber.y, pBomber.nombre);
+                pBomber.cambiarTipo("blackwithbomb1");
+                Gestor.getInstance().getTablero().aniadirContent(pBomber.x, pBomber.y, pBomber);
+                pBomber.cambiarTipo("bombermanN");
+                Gestor.getInstance().getTablero().aniadirContent(pBomber.x, pBomber.y, new BombaUltra(pBomber.x, pBomber.y));
+                pBomber.bombasActivas++;
+                System.out.println("BOMBA ULTRA colocada. Activas: " + pBomber.bombasActivas);
+            }
+        }
+    }
+}
